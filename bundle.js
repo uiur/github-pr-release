87171fea519d5112e087b9851bc7f26dcceaaef2
Kazato Sugimoto <uiureo@gmail.com>
2015-01-22 15:19:21 +0900

    Refactor

--- bundle.js
+++ bundle.js
@@ -16,6 +16,7 @@ var Github = require("github"),
 var github = new Github({
   version: "3.0.0"
 });
+var pullRequests = thenifyAll(github.pullRequests);
 
 var defaultConfig = {
   branch: {
@@ -23,6 +24,15 @@ var defaultConfig = {
     staging: "staging"
   }
 };
+
+var config = null;
+
+function getRepo() {
+  return {
+    user: config.repo.split("/")[0],
+    repo: config.repo.split("/")[1]
+  };
+}
 
 var readConfig = co.wrap(regeneratorRuntime.mark(function callee$0$0() {
   var userConfig;
@@ -41,15 +51,80 @@ var readConfig = co.wrap(regeneratorRuntime.mark(function callee$0$0() {
   }, callee$0$0, this);
 }));
 
-module.exports = co.wrap(regeneratorRuntime.mark(function callee$0$1() {
-  var config, pullRequests, repo, targetPR, commits, shas, prs, releasePRs, template, text, _text$split, _text$split2, title, body;
+var createReleaseMessage = co.wrap(regeneratorRuntime.mark(function callee$0$1(prs) {
+  var template, text, _text$split, _text$split2, title, body;
   return regeneratorRuntime.wrap(function callee$0$1$(context$1$0) {
+    while (1) switch (context$1$0.prev = context$1$0.next) {
+      case 0:
+        context$1$0.next = 2;
+        return fs.readFile(__dirname + "/release.mustache", "utf8");
+      case 2:
+        template = context$1$0.sent;
+        text = render(template, { now: moment().format("YYYY-MM-DD HH:mm:ss"), prs: prs });
+        _text$split = text.split("\n");
+        _text$split2 = _toArray(_text$split);
+        title = _text$split2[0];
+        body = _toArray(_text$split2).slice(1);
+        return context$1$0.abrupt("return", {
+          title: title,
+          body: body.join("\n")
+        });
+      case 9:
+      case "end":
+        return context$1$0.stop();
+    }
+  }, callee$0$1, this);
+}));
+
+var getReleasePRs = co.wrap(regeneratorRuntime.mark(function callee$0$2(targetPR) {
+  var repo, commits, shas, prs, releasePRs;
+  return regeneratorRuntime.wrap(function callee$0$2$(context$1$0) {
+    while (1) switch (context$1$0.prev = context$1$0.next) {
+      case 0:
+        repo = getRepo();
+        context$1$0.next = 3;
+        return pullRequests.getCommits(Object.assign({}, repo, {
+          number: targetPR.number,
+          per_page: 100
+        }));
+      case 3:
+        commits = context$1$0.sent;
+        shas = commits.map(function (commit) {
+          return commit.sha;
+        });
+        context$1$0.next = 7;
+        return pullRequests.getAll(Object.assign({}, repo, {
+          state: "closed",
+          per_page: 100
+        }));
+      case 7:
+        prs = context$1$0.sent;
+        releasePRs = prs.reduce(function (ret, pr) {
+          var matched = pr.number !== targetPR.number && pr.base.ref === targetPR.head.ref;
+
+          if (shas.indexOf(pr.head.sha) !== -1 && matched) {
+            ret.push(pr);
+          }
+          return ret;
+        }, []);
+        return context$1$0.abrupt("return", releasePRs);
+      case 10:
+      case "end":
+        return context$1$0.stop();
+    }
+  }, callee$0$2, this);
+}));
+
+module.exports = co.wrap(regeneratorRuntime.mark(function callee$0$3() {
+  var repo, targetPR, releasePRs, release;
+  return regeneratorRuntime.wrap(function callee$0$3$(context$1$0) {
     while (1) switch (context$1$0.prev = context$1$0.next) {
       case 0:
         context$1$0.next = 2;
         return readConfig();
       case 2:
         config = context$1$0.sent;
+        repo = getRepo();
 
 
         github.authenticate({
@@ -57,62 +132,43 @@ module.exports = co.wrap(regeneratorRuntime.mark(function callee$0$1() {
           token: config.token
         });
 
-        pullRequests = thenifyAll(github.pullRequests);
-        repo = {
-          user: config.repo.split("/")[0],
-          repo: config.repo.split("/")[1]
-        };
-        context$1$0.next = 8;
+        context$1$0.next = 7;
         return pullRequests.create(Object.assign({}, repo, {
           title: "Preparing release pull request...",
           base: config.branch.production,
           head: config.branch.staging
         }));
-      case 8:
+      case 7:
         targetPR = context$1$0.sent;
-        context$1$0.next = 11;
-        return pullRequests.getCommits(Object.assign({}, repo, {
-          number: targetPR.number,
-          per_page: 100
-        }));
-      case 11:
-        commits = context$1$0.sent;
-        shas = commits.map(function (commit) {
-          return commit.sha;
-        });
-        context$1$0.next = 15;
-        return pullRequests.getAll(Object.assign({}, repo, {
-          state: "closed",
-          per_page: 100
-        }));
-      case 15:
-        prs = context$1$0.sent;
-        releasePRs = prs.reduce(function (ret, pr) {
-          var matched = pr.number !== targetPR.number && pr.base.ref === config.branch.staging;
-
-          if (shas.indexOf(pr.head.sha) !== -1 && matched) {
-            ret.push(pr);
-          }
-          return ret;
-        }, []);
-        context$1$0.next = 19;
-        return fs.readFile(__dirname + "/release.mustache", "utf8");
-      case 19:
-        template = context$1$0.sent;
-        text = render(template, { now: moment().format("YYYY-MM-DD HH:mm:ss"), prs: releasePRs });
-        _text$split = text.split("\n");
-        _text$split2 = _toArray(_text$split);
-        title = _text$split2[0];
-        body = _toArray(_text$split2).slice(1);
-        context$1$0.next = 27;
+        context$1$0.next = 10;
+        return getReleasePRs(targetPR);
+      case 10:
+        releasePRs = context$1$0.sent;
+        context$1$0.next = 13;
+        return createReleaseMessage(releasePRs);
+      case 13:
+        release = context$1$0.sent;
+        context$1$0.next = 16;
         return pullRequests.update(Object.assign({}, repo, {
           number: targetPR.number,
-          title: title,
-          body: body.join("\n")
+          title: release.title,
+          body: release.body
         }));
-      case 27:
+      case 16:
       case "end":
         return context$1$0.stop();
     }
-  }, callee$0$1, this);
+  }, callee$0$3, this);
 }));
+
+
+// TODO: debug
+// var targetPR = {
+//   number: 1089,
+//   base: {
+//     ref: config.branch.production
+//   },
+//   head: {
+//     ref: config.branch.staging
+//   }
+// }
--- index.js
+++ index.js
@@ -10,11 +10,21 @@ var Github = require('github')
 var github = new Github({
   version: '3.0.0'
 })
+var pullRequests = thenifyAll(github.pullRequests)
 
 const defaultConfig = {
   branch: {
     production: 'master',
     staging: 'staging'
+  }
+}
+
+var config = null
+
+function getRepo() {
+  return {
+    user: config.repo.split('/')[0],
+    repo: config.repo.split('/')[1]
   }
 }
 
@@ -24,26 +34,20 @@ var readConfig = co.wrap(function* () {
   return Object.assign({}, defaultConfig, userConfig)
 })
 
-module.exports = co.wrap(function* () {
-  var config = yield readConfig()
+var createReleaseMessage = co.wrap(function* (prs) {
+  var template = yield fs.readFile(__dirname + '/release.mustache', 'utf8')
 
-  github.authenticate({
-    type: 'oauth',
-    token: config.token
-  })
+  var text = render(template, { now: moment().format('YYYY-MM-DD HH:mm:ss'), prs: prs })
+  var [title, ...body] = text.split('\n')
 
-  var pullRequests = thenifyAll(github.pullRequests)
-
-  var repo = {
-    user: config.repo.split('/')[0],
-    repo: config.repo.split('/')[1]
+  return {
+    title: title,
+    body: body.join('\n')
   }
+})
 
-  var targetPR = yield pullRequests.create(Object.assign({}, repo, {
-    title: 'Preparing release pull request...',
-    base: config.branch.production,
-    head: config.branch.staging
-  }))
+var getReleasePRs = co.wrap(function* (targetPR) {
+  var repo = getRepo()
 
   var commits = yield pullRequests.getCommits(Object.assign({}, repo, {
     number: targetPR.number,
@@ -58,7 +62,7 @@ module.exports = co.wrap(function* () {
   }))
 
   var releasePRs = prs.reduce((ret, pr) => {
-    var matched =  pr.number !== targetPR.number && pr.base.ref === config.branch.staging
+    var matched =  pr.number !== targetPR.number && pr.base.ref === targetPR.head.ref
 
     if (shas.indexOf(pr.head.sha) !== -1 && matched) {
       ret.push(pr)
@@ -66,14 +70,43 @@ module.exports = co.wrap(function* () {
     return ret
   }, [])
 
-  var template = yield fs.readFile(__dirname + '/release.mustache', 'utf8')
+  return releasePRs
+})
 
-  var text = render(template, { now: moment().format('YYYY-MM-DD HH:mm:ss'), prs: releasePRs })
-  var [title, ...body] = text.split('\n')
+module.exports = co.wrap(function* () {
+  config = yield readConfig()
+
+  var repo = getRepo()
+
+  github.authenticate({
+    type: 'oauth',
+    token: config.token
+  })
+
+  var targetPR = yield pullRequests.create(Object.assign({}, repo, {
+    title: 'Preparing release pull request...',
+    base: config.branch.production,
+    head: config.branch.staging
+  }))
+
+  // TODO: debug
+  // var targetPR = {
+  //   number: 1089,
+  //   base: {
+  //     ref: config.branch.production
+  //   },
+  //   head: {
+  //     ref: config.branch.staging
+  //   }
+  // }
+
+  var releasePRs = yield getReleasePRs(targetPR)
+
+  var release = yield createReleaseMessage(releasePRs)
 
   yield pullRequests.update(Object.assign({}, repo, {
     number: targetPR.number,
-    title: title,
-    body: body.join('\n')
+    title: release.title,
+    body: release.body
   }))
 })
