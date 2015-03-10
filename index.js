5dd8ef3f338119dc7ed95e086d6aa5f735ccc9b1
Kazato Sugimoto <uiureo@gmail.com>
2015-03-10 11:48:00 +0900

    6to5 -> babel

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 "use strict";
 
-var _toArray = function (arr) {
-  return Array.isArray(arr) ? arr : Array.from(arr);
-};
+var _toArray = function (arr) { return Array.isArray(arr) ? arr : Array.from(arr); };
 
 require("6to5/polyfill");
 
@@ -42,17 +40,19 @@ function readConfigFile() {
 }
 
 var readConfig = co.wrap(regeneratorRuntime.mark(function callee$0$0() {
-  var runtimeConfig, fileConfig, args$1$0 = arguments;
+  var runtimeConfig = arguments[0] === undefined ? {} : arguments[0];
+  var fileConfig;
   return regeneratorRuntime.wrap(function callee$0$0$(context$1$0) {
     while (1) switch (context$1$0.prev = context$1$0.next) {
       case 0:
-        runtimeConfig = args$1$0[0] === undefined ? {} : args$1$0[0];
-        context$1$0.next = 3;
+        context$1$0.next = 2;
         return readConfigFile();
-      case 3:
+
+      case 2:
         fileConfig = context$1$0.sent;
         return context$1$0.abrupt("return", Object.assign({}, defaultConfig, fileConfig, runtimeConfig));
-      case 5:
+
+      case 4:
       case "end":
         return context$1$0.stop();
     }
@@ -61,22 +61,25 @@ var readConfig = co.wrap(regeneratorRuntime.mark(function callee$0$0() {
 
 var createReleaseMessage = co.wrap(regeneratorRuntime.mark(function callee$0$1(prs) {
   var template, text, _text$split, _text$split2, title, body;
+
   return regeneratorRuntime.wrap(function callee$0$1$(context$1$0) {
     while (1) switch (context$1$0.prev = context$1$0.next) {
       case 0:
         context$1$0.next = 2;
         return fs.readFile(__dirname + "/release.mustache", "utf8");
+
       case 2:
         template = context$1$0.sent;
         text = render(template, { now: moment().format("YYYY-MM-DD HH:mm:ss"), prs: prs });
         _text$split = text.split("\n");
         _text$split2 = _toArray(_text$split);
         title = _text$split2[0];
-        body = _toArray(_text$split2).slice(1);
+        body = _text$split2.slice(1);
         return context$1$0.abrupt("return", {
           title: title,
           body: body.join("\n")
         });
+
       case 9:
       case "end":
         return context$1$0.stop();
@@ -85,8 +88,11 @@ var createReleaseMessage = co.wrap(regeneratorRuntime.mark(function callee$0$1(p
 }));
 
 function getPRCommits(repo, targetPR) {
-  var getCommits = function () {
+  var result = [];
+
+  function getCommits() {
     var page = arguments[0] === undefined ? 1 : arguments[0];
+
     return pullRequests.getCommits(Object.assign({}, repo, {
       number: targetPR.number,
       per_page: 100,
@@ -102,9 +108,7 @@ function getPRCommits(repo, targetPR) {
         return result;
       }
     });
-  };
-
-  var result = [];
+  }
 
   return getCommits();
 }
@@ -117,6 +121,7 @@ var getReleasePRs = co.wrap(regeneratorRuntime.mark(function callee$0$2(targetPR
         repo = getRepo();
         context$1$0.next = 3;
         return getPRCommits(repo, targetPR);
+
       case 3:
         commits = context$1$0.sent;
         shas = commits.map(function (commit) {
@@ -127,6 +132,7 @@ var getReleasePRs = co.wrap(regeneratorRuntime.mark(function callee$0$2(targetPR
           state: "closed",
           per_page: 100
         }));
+
       case 7:
         prs = context$1$0.sent;
         mergedPRs = prs.filter(function (pr) {
@@ -141,6 +147,7 @@ var getReleasePRs = co.wrap(regeneratorRuntime.mark(function callee$0$2(targetPR
           return ret;
         }, []);
         return context$1$0.abrupt("return", releasePRs);
+
       case 11:
       case "end":
         return context$1$0.stop();
@@ -149,46 +156,50 @@ var getReleasePRs = co.wrap(regeneratorRuntime.mark(function callee$0$2(targetPR
 }));
 
 module.exports = co.wrap(regeneratorRuntime.mark(function callee$0$3() {
-  var runtimeConfig, repo, targetPR, releasePRs, release, args$1$0 = arguments;
+  var runtimeConfig = arguments[0] === undefined ? {} : arguments[0];
+  var repo, targetPR, releasePRs, release;
   return regeneratorRuntime.wrap(function callee$0$3$(context$1$0) {
     while (1) switch (context$1$0.prev = context$1$0.next) {
       case 0:
-        runtimeConfig = args$1$0[0] === undefined ? {} : args$1$0[0];
-        context$1$0.next = 3;
+        context$1$0.next = 2;
         return readConfig(runtimeConfig);
-      case 3:
+
+      case 2:
         config = context$1$0.sent;
         repo = getRepo();
-
 
         github.authenticate({
           type: "oauth",
           token: config.token
         });
 
-        context$1$0.next = 8;
+        context$1$0.next = 7;
         return pullRequests.create(Object.assign({}, repo, {
           title: "Preparing release pull request...",
           base: config.branch.production,
           head: config.branch.staging
         }));
-      case 8:
+
+      case 7:
         targetPR = context$1$0.sent;
-        context$1$0.next = 11;
+        context$1$0.next = 10;
         return getReleasePRs(targetPR);
-      case 11:
+
+      case 10:
         releasePRs = context$1$0.sent;
-        context$1$0.next = 14;
+        context$1$0.next = 13;
         return createReleaseMessage(releasePRs);
-      case 14:
+
+      case 13:
         release = context$1$0.sent;
-        context$1$0.next = 17;
+        context$1$0.next = 16;
         return pullRequests.update(Object.assign({}, repo, {
           number: targetPR.number,
           title: release.title,
           body: release.body
         }));
-      case 17:
+
+      case 16:
       case "end":
         return context$1$0.stop();
     }
