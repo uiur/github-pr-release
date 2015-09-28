884b7c91498107aecb67e41493c473c4324bf6db
Kazato Sugimoto <uiureo@gmail.com>
2015-09-28 11:35:47 +0900

    Add test for release()

--- test/index.js
+++ test/index.js
@@ -1,9 +1,35 @@
 /* global describe it */
 var assert = require('power-assert')
+var proxyquire = require('proxyquire')
+var Promise = require('es6-promise').Promise
+
+function MockGithubClient () {
+  return {
+    prepareReleasePR: function () {
+      return Promise.resolve({ number: 42 })
+    },
+    collectReleasePRs: function () {
+      return Promise.resolve([
+        { number: 42, title: 'foo', user: { login: 'uiureo' } },
+        { number: 43, title: 'bar', user: { login: 'hiroshi' } }
+      ])
+    },
+    updatePR: function (releasePR, message) {
+      return Promise.resolve({ pr: releasePR, message: message })
+    }
+  }
+}
+proxyquire('../', {
+  './lib/github-client.js': MockGithubClient
+})
 var release = require('../')
 
 describe('release()', function () {
-  it('is a func', function () {
-    assert(typeof release === 'function')
+  it('generates a PR message', function (done) {
+    release({}).then(function (result) {
+      assert((/^Release/).test(result.message.title))
+      assert((/#42 foo @uiureo/).test(result.message.body))
+      assert((/#43 bar @hiroshi/).test(result.message.body))
+    }).then(done, done)
   })
 })
